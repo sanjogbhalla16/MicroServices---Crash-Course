cc1b1179399334c5c1f37167d8428a567368120d
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-01-20 13:36:44 +0530

    Micro-main

--- Micro-main/main.py
+++ Micro-main/main.py
@@ -1,15 +1,39 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+redis = get_redis_connection(
+    host="redis-11455.c16.us-east-1-3.ec2.redns.redis-cloud.com",
+    port=11455,
+    password="ywV3wyoyawqmv749vAqxQWoi5uFjU7et",
+    decode_responses=True
+)
 
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+    
+    class Meta:
+        database = redis
+    
+    class Config:
+        arbitrary_types_allowed = True
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/products")
+def all():
+    return Product.all_pks()
+
+@app.post("/products")
+def create_product(product: Product):
+    return product.save()
