a8d78a5a8921bf1f2f17fafd318313d05438a933
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-01-20 18:07:09 +0530

    Micro-Product

--- Micro-product/main.py
+++ Micro-product/main.py
@@ -40,8 +40,25 @@ class Product(HashModel):
 
 @app.get("/products")
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+    return {
+        "id": product.pk,
+        "name": product.name,
+        "price": product.price,
+        "quantity": product.quantity
+    }
 
 @app.post("/products")
 def create_product(product: Product):
     return product.save()
+
+@app.get("/products/{pk}")
+def get_product(pk: str):
+    return Product.get(pk)
+
+@app.delete("/products/{pk}")
+def delete_product(pk: str):
+    return Product.delete(pk)
