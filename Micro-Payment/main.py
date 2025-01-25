f2a3ca7bab020377de4aebaa6d63218262258828
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-01-25 14:10:55 +0530

    Micro-Payment

--- Micro-Payment/main.py
+++ Micro-Payment/main.py
@@ -2,7 +2,8 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from redis_om import get_redis_connection
+from redis_om import get_redis_connection,HashModel
+from starlette.requests import Request
 import requests
 import uvicorn
 import os
@@ -24,3 +25,32 @@ redis = get_redis_connection(
     password=os.getenv("REDIS_PASSWORD"),
     decode_responses=True
 )
+
+# now we make the orders
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: str
+    total: float
+    quantity: int
+    status: str # pending, completed or failed
+
+    class Config:
+        database = redis
+
+@app.post("/orders")
+async def create_order(request: Request): #id and quantity
+    body = await request.json()
+    
+    #we are getting this product from another microservice so we use requests
+    request = requests.get("http://localhost:8000/products/%s" % body["id"])
+    # product = request.json()
+    # order = Order(
+    #     product_id=body["id"],
+    #     quantity=body["quantity"],
+    #     price=product["price"],
+    #     fee=0.2 * product["price"],
+    #     total=1.2 * product["price"]
+    # )
+    # order.save()
+    return request.json()
